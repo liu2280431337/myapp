3f12e9d4402b3ff3f5f18224fe2970d5e3930cb0
liugenhao <2280431337@qq.com>
2022-12-06 16:43:23 +0800

    默认样式设置

--- src/App.vue
+++ src/App.vue
@@ -4,4 +4,5 @@
 </template>
 
 <style lang="scss">
+
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 设置css的主入口文件
+  css:{
+    loaderOptions: {
+      scss: {
+        additionalData: `@import "./src/styles/style.scss";`
+      }
+    }
+  }
 })

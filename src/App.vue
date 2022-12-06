eaf2c92583aed02117ec1514cefbb3ef5eb38511
liugenhao <2280431337@qq.com>
2022-12-06 15:55:47 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <!-- 路由出口 -->
+  <router-view/>
+</template>
+
+<style lang="scss">
+</style>
---
+++ src/views/login/LoginView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="login">登录页</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "LoginView"
+}
+</script>
+
+<style lang="scss">
+.login {
+  background-color: #000066;
+  height: 100%;
+}
+html,body,#app {
+  height: 100%;
+}
+</style>
